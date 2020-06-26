b4aa5efd86d7cdc7e92dc84d24cfae1872a8c65f
Darian Tavana <dtavana@fb.com>
2020-06-26 16:16:36 -0400

    Add model fromJson methods to create models from the API requests

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,4 +1,58 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tweet {
+    String body;
+    String createdAt;
+    User user;
+
+    public static Tweet fromJson(JSONObject obj) throws JSONException {
+        Tweet tweet = new Tweet();
+
+        tweet.setBody(obj.getString("text"));
+        tweet.setCreatedAt(obj.getString("created_at"));
+        tweet.setUser(User.fromJson(obj.getJSONObject("user")));
+
+        return tweet;
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray arr) throws JSONException {
+        List<Tweet> res = new ArrayList<>();
+
+        for(int i = 0; i < arr.length(); i++) {
+            res.add(fromJson(arr.getJSONObject(i)));
+        }
+
+        return res;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,4 +1,46 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class User {
+
+    String name;
+    String screenName;
+    String imageUrl;
+
+
+    public static User fromJson(JSONObject obj) throws JSONException {
+        User user = new User();
+
+        user.setName(obj.getString("name"));
+        user.setScreenName(obj.getString("screen_name"));
+        user.setImageUrl(obj.getString("profile_image_url_https"));
+
+        return user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
